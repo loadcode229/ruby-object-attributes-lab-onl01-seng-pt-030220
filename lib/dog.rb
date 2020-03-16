9fc96bb5ced0b6f152460d84ca1546a7834a3bcb
Adam Priest <sepone229@gmail.com>
2020-03-16 10:52:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,14 @@
 class Dog
   def name=(dogs_name)
-    @this_dogs_name = dogs_name
+    @name = dogs_name
   end
   def name
-    @this_dogs_name
+    @name
   end
-  puts name
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end 
+  def breed
+    @breed
+  end
 end
